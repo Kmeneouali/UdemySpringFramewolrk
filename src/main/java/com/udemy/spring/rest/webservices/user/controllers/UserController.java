83570dba8f39adff84d496089b210f2621f2a23f
Kmeneouali <khalledmeneouali89@gmail.com>
2021-08-11 09:19:18 +0200

    Enhancing POST Method to return correct HTTP

--- src/main/java/com/udemy/spring/rest/webservices/user/controllers/UserController.java
+++ src/main/java/com/udemy/spring/rest/webservices/user/controllers/UserController.java
@@ -1,13 +1,16 @@
 package com.udemy.spring.rest.webservices.user.controllers;
 
+import java.net.URI;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.udemy.spring.rest.webservices.user.beans.User;
 import com.udemy.spring.rest.webservices.user.dao.userDaoService;
@@ -30,7 +33,12 @@ public class UserController {
 	}
 	
 	@PostMapping("/users")
-	public void createUser(@RequestBody User user) {
+	public ResponseEntity<Object> createUser(@RequestBody User user) {
 		User createdUser=services.save(user);
+		
+	   URI location= ServletUriComponentsBuilder.fromCurrentRequest().path("{id}").buildAndExpand(createdUser.getId()).toUri();
+	   
+	   
+	   return ResponseEntity.created(location).build();
 	}
 }
