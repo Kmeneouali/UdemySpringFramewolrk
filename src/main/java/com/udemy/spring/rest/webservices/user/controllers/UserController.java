c589a9e6ba003b532902288ffa3820a0d337c813
Kmeneouali <khalledmeneouali89@gmail.com>
2021-08-12 08:54:31 +0200

    Added DELETE Method

--- src/main/java/com/udemy/spring/rest/webservices/user/controllers/UserController.java
+++ src/main/java/com/udemy/spring/rest/webservices/user/controllers/UserController.java
@@ -5,6 +5,7 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -18,29 +19,38 @@ import com.udemy.spring.rest.webservices.user.dao.userDaoService;
 
 @RestController
 public class UserController {
-	
-	
+
 	@Autowired
 	userDaoService services;
 
 	@GetMapping("/users")
-	public List<User> retrieveAllUser(){
+	public List<User> retrieveAllUser() {
 		return services.findAll();
 	}
-	
+
 	@GetMapping("/users/{id}")
 	public User retrieveUserById(@PathVariable Integer id) {
-		User user=services.findOne(id);
-		
-		if(user==null)
-			throw new UserNotFoundException("id - "+id);
+		User user = services.findOne(id);
+
+		if (user == null)
+			throw new UserNotFoundException("id - " + id);
 		return user;
 	}
-	
+
 	@PostMapping("/users")
 	public ResponseEntity<Object> createUser(@RequestBody User user) {
-		User createdUser=services.save(user);
-	   URI location= ServletUriComponentsBuilder.fromCurrentRequest().path("{id}").buildAndExpand(createdUser.getId()).toUri();
-	   return ResponseEntity.created(location).build();
+		User createdUser = services.save(user);
+		URI location = ServletUriComponentsBuilder.fromCurrentRequest().path("{id}").buildAndExpand(createdUser.getId())
+				.toUri();
+		return ResponseEntity.created(location).build();
+	}
+
+	@DeleteMapping("/users/{id}")
+	public void deleteUserById(@PathVariable Integer id) {
+		User user = services.deleteUserById(id);
+
+		if (user == null)
+			throw new UserNotFoundException("id - " + id);
+
 	}
 }
--- src/main/java/com/udemy/spring/rest/webservices/user/dao/userDaoService.java
+++ src/main/java/com/udemy/spring/rest/webservices/user/dao/userDaoService.java
@@ -10,35 +10,46 @@ import com.udemy.spring.rest.webservices.user.beans.User;
 
 @Component
 public class userDaoService {
-	private static int userCount=3;
-	private static List<User> users=new ArrayList<User>();
-	
+	private static int userCount = 3;
+	private static List<User> users = new ArrayList<User>();
+
 	static {
-		
+
 		users.add(new User(1, "khalled", new Date()));
 		users.add(new User(2, "Omar", new Date()));
 		users.add(new User(3, "Ali", new Date()));
-		
+
 	}
-	
-	public List<User> findAll(){
+
+	public List<User> findAll() {
 		return users;
 	}
-	
+
 	public User save(User user) {
-		if(user.getId()==null) {
+		if (user.getId() == null) {
 			user.setId(++userCount);
 		}
 		users.add(user);
 		return user;
-		
+
 	}
-	
+
 	public User findOne(Integer id) {
-		for(User u:users) {
-			if(u.getId()==id) {
+		for (User u : users) {
+			if (u.getId() == id) {
 				return u;
 			}
+		}
+		return null;
+	}
+
+	public User deleteUserById(Integer id) {
+		for (User u : users) {
+			if (u.getId() == id) {
+				users.remove(u);
+				return u;
+			}
+
 		}
 		return null;
 	}
